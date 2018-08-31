f0a5269ab721581467395caf9f28424c3bea522e
Ashwin Karangutkar <ashwin.karangutkar@gmail.com>
2018-08-31 15:05:43 +0530

    For each method

---
+++ src/com/forEachMethod/TestForEachMethod.java
@@ -0,0 +1,20 @@
+package com.forEachMethod;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestForEachMethod {
+
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(10);
+        numbers.add(20);
+        numbers.add(30);
+        numbers.add(40);
+        numbers.add(50);
+
+        numbers.forEach(number -> System.out.println(number));
+    }
+}
